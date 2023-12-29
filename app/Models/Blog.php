6ba9949bcdf0a25b4ca8b02afd17acda4e7559f6
soe <sithu032001@gmail.com>
2023-12-29 11:02:02 +0630

    'scopeSearch'

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -12,12 +12,32 @@ class Blog extends Model
     function scopeSearch($query, $searchValue)
     {
 
-        if ($searchValue) {
-            $query->where('title', 'Like', '%' . $searchValue . '%');
-        }
-        // $blogInstance =
+        // First way
+        // if ($searchValue) {
+        //     $query
+        //     ->where('title', 'Like', '%' . $searchValue . '%')
+        //     ->orWhere('body', 'Like', '%' . $searchValue . '%');
+        // }
+        // return $query;
 
-        return $query;
+
+        //Second way
+        // return $query->when($searchValue, function ($query) use ($searchValue) {
+
+        //     $query->where('title', 'Like', '%' . $searchValue . '%')
+        //         ->orWhere('body', 'Like', '%' . $searchValue . '%');
+        // });
+
+
+        //Second way + logical grouping
+        return $query
+            ->when($searchValue, function ($query) use ($searchValue) {
+                $query
+                    ->where(function ($query) use ($searchValue) {
+                        $query->where('title', 'Like', '%' . $searchValue . '%')
+                            ->orWhere('body', 'Like', '%' . $searchValue . '%');
+                    });
+            });
     }
 
     public function category()
